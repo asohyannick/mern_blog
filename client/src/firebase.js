eba607aef1a4798cb49f7766b1da52eabb66a4fc
asohyannick <asohyannick3@gmail.com>
2024-02-18 14:24:29 -0800

    create user model

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD31_TkQfCDWXWHjlCwBH_P_HIvjqdjXZg",
+  authDomain: "mern-app-e0924.firebaseapp.com",
+  projectId: "mern-app-e0924",
+  storageBucket: "mern-app-e0924.appspot.com",
+  messagingSenderId: "298382809595",
+  appId: "1:298382809595:web:f0c78d93fda1629d93935e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
