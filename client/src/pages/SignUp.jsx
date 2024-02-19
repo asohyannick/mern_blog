305e4fff3b6aa6564236f652b5a401a34a0dafb3
asohyannick <asohyannick3@gmail.com>
2024-02-19 15:07:24 -0800

    add functionality to the github page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {Label, TextInput, Button} from 'flowbite-react';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
@@ -14,48 +47,67 @@ const SignUp = () => {
             Blog
           </Link>
           <p className="text-sm mt-5">
-            Welcome to my ultimate MERN Stack web developement blog. Please, you can sign up
-            with your username and password to get latest weekly information
-            about the MERN stack from my blog.
+            Welcome to my ultimate MERN Stack web developement blog. Please, you
+            can sign up with your username and password to get latest weekly
+            information about the MERN stack from my blog.
           </p>
         </div>
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value='Enter your username'/>
+              <Label value="Enter your username" />
               <TextInput
-                type='text'
-                placeholder='Enter your username'
+                type="text"
+                placeholder="Enter your username"
                 id="username"
+                onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Enter your email address'/>
+              <Label value="Enter your email address" />
               <TextInput
-                type='email'
-                placeholder='Enter your email'
+                type="email"
+                placeholder="Enter your email"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Enter your password'/>
+              <Label value="Enter your password" />
               <TextInput
-                type='password'
-                placeholder='Enter your password'
+                type="password"
+                placeholder="Enter your password"
                 id="password"
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className="text-blue-500">
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
         <div />
       </div>
