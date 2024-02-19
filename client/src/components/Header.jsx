6a784015070f48f6c31e87d17d638a71465ecedf
asohyannick <asohyannick3@gmail.com>
2024-02-19 13:09:49 -0800

    complete signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Navbar, TextInput, Button } from "flowbite-react";
-import { Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 const Header = () => {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
@@ -32,20 +32,20 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-            <Button gradientDuoTone="purpleToBlue" outline>
-                Sign In
-            </Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
       </Navbar.Collapse>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import {Label, TextInput, Button} from 'flowbite-react';
 const SignUp = () => {
   return (
-    <div>
-       signup page
+    <div className="min-h-screen mt-20">
+      <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+        <div className="flex-1">
+          {/* left */}
+          <Link to="/" className=" text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Asoh Yannick's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Welcome to my ultimate MERN Stack web developement blog. Please, you can sign up
+            with your username and password to get latest weekly information
+            about the MERN stack from my blog.
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value='Enter your username'/>
+              <TextInput
+                type='text'
+                placeholder='Enter your username'
+                id="username"
+              />
+            </div>
+            <div>
+              <Label value='Enter your email address'/>
+              <TextInput
+                type='email'
+                placeholder='Enter your email'
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value='Enter your password'/>
+              <TextInput
+                type='password'
+                placeholder='Enter your password'
+                id="password"
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/sign-in' className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+        <div />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
