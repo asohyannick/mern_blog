b7c1f4dad2eada6171ef3addcea0dcc5053bd2cc
asohyannick <asohyannick3@gmail.com>
2024-02-21 03:53:38 -0800

    create getposts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -18,15 +18,55 @@ export const create = async (req, res, next) => {
     .join("-")
     .toLowerCase()
     .replace(/[^a-zA-z0-9-]/g, "");
-    const newPost = new Post({
-        ...req.body,
-        slug,
-        userId: req.user.id,
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(StatusCodes.CREATED).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.params.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { userId: req.query.category }),
+      ...(req.query.slug && { userId: req.query.slug }),
+      ...(req.query.postId && { userId: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
     });
-    try{
-        const savedPost = await newPost.save();
-        res.status(StatusCodes.CREATED).json(savedPost);
-    } catch(error) {
-        next(error);
-    }
+    res.status(StatusCodes.OK).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
